055ddae9581ea4cd2a295d635ca915bb6cfd0fd4
sara-khaled1 <saraaljaber501@gmail.com>
2024-05-06 15:13:40 +0300

    test

--- src/Entity/Category.cs
+++ src/Entity/Category.cs
@@ -1,13 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace sda_onsite_2_csharp_backend_teamwork.src.Entity
 {
     public class Category
     {
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
 
--- src/Entity/OrderItem.cs
+++ src/Entity/OrderItem.cs
@@ -1,13 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace sda_onsite_2_csharp_backend_teamwork.src.Entity
 {
     public class OrderItem
     {
         public Guid Id { get; set; }
+        [Required]
         public Guid OrderId { get; set; }
         public Guid ProductId { get; set; }
         public int Quantity { get; set; }
