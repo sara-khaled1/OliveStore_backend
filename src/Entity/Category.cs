2507bf4ad38d88ea9fd3d6c5fb654b13cd9acebb
sara-khaled1 <saraaljaber501@gmail.com>
2024-05-07 11:33:30 +0300

    [Required]

--- src/Entity/Category.cs
+++ src/Entity/Category.cs
@@ -4,11 +4,12 @@ namespace sda_onsite_2_csharp_backend_teamwork.src.Entity
 {
     public class Category
     {
+        [Required]
         public Guid Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
-
         public IEnumerable<Product> Products { get; set; }
 
     }
--- src/Entity/OrderItem.cs
+++ src/Entity/OrderItem.cs
@@ -4,11 +4,15 @@ namespace sda_onsite_2_csharp_backend_teamwork.src.Entity
 {
     public class OrderItem
     {
+        [Required]
         public Guid Id { get; set; }
         [Required]
         public Guid OrderId { get; set; }
+        [Required]
         public Guid ProductId { get; set; }
+        [Required]
         public int Quantity { get; set; }
+        [Required]
         public double UnitPrice { get; set; }
 
     }
