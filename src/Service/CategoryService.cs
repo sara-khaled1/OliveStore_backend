86e725e654b75adf359351a958591f351633e2dc
sara-khaled1 <saraaljaber501@gmail.com>
2024-05-08 14:07:23 +0300

    Exception Category

--- src/Service/CategoryService.cs
+++ src/Service/CategoryService.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using sda_onsite_2_csharp_backend_teamwork.src.Abstraction;
 using sda_onsite_2_csharp_backend_teamwork.src.Entity;
 using sda_onsite_2_csharp_backend_teamwork.src.DTO;
+using sda_onsite_2_csharp_backend_teamwork.src.Exceptions;
 namespace sda_onsite_2_csharp_backend_teamwork.src.Service;
 public class CategoryService : ICategoryService
 {
@@ -22,17 +23,23 @@ public class CategoryService : ICategoryService
     public CategoryReadDto? FindOne(Guid id)
     {
         Category? category = _categoryRepository.FindOne(id);
+
+        if (category is null)
+        {
+            throw CustomErrorException.NotFound("item is not found");
+        }
+
         CategoryReadDto? categoryRead = _mapper.Map<CategoryReadDto>(category);
         return categoryRead;
     }
     public CategoryReadDto CreateOne(CategoryCreateDto category)
     {
-        var newCategory=_mapper.Map<Category>(category);
+        var newCategory = _mapper.Map<Category>(category);
         var createdCatgory = _categoryRepository.CreateOne(newCategory);
         var categoryRead = _mapper.Map<CategoryReadDto>(createdCatgory);
         return categoryRead;
     }
-    
+
     public CategoryReadDto? UpdateOne(Guid categoryId, Category newCategory)
     {
         Category? updatedCategory = _categoryRepository.FindOne(categoryId);
@@ -44,11 +51,16 @@ public class CategoryService : ICategoryService
             var updatedCategoryRead = _mapper.Map<CategoryReadDto>(categoryUpdated);
             return updatedCategoryRead;
         }
-        else return null;
+        throw new ArgumentException("item is not found");
     }
 
     public bool DeleteOne(Guid id)
     {
-        return _categoryRepository.DeleteOne(id);
+        var deleted = _categoryRepository.DeleteOne(id);
+        if (!deleted)
+        {
+            throw new ArgumentException("Item is not found");
+        }
+        return true;
     }
 }
